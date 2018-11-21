1a10a7a80f7eac275568f6fa858fbbb97485131d
Abhishek Anand <38378778+Abhi-2109@users.noreply.github.com>
2018-11-21 21:23:17 +0530

    Added Maximum Subarray Sum Problem

---
+++ Maximum SubArray Sum Problem/maxSubarraySum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n ;i++)
+        cin >> a[i];
+    int maxSum=INT_MIN,currentSum = 0;
+
+    // This is also called kadane Algorithm
+
+
+    for(int i=0; i < n; i++)
+    {
+        currentSum += arr[i];
+        maxSum = max(maxSum,currentSum);
+        currentSum = max(currentSum,0);
+    }
+    cout << maxSum;
+}
+
